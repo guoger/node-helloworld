dea254192a22ce2a9c532498f7f1704bfa9236b2
Jay Guo <guojiannan1101@gmail.com>
2018-05-31 16:46:38 +0800

    WIP

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var express = require("express"),
-    app = express();
+  app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/sayHello", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+app.post("/register", function (req, res) {
+  console.log(req.query);
+  res.status(200).send("Hello " + req.query.email + "!");
 });
 
 app.listen(port);
 console.log("Listening on port ", port);
-
-require("cf-deployment-tracker-client").track();
